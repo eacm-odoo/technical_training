5729bdade46fc01e5ad116030d24a1f084d122da
EduardoCedillo(eacm) <eacm@odoo.com>
2022-06-13 16:49:44 +0000

    [ADD] Basic Volunteer Module

---
+++ odoo_volunteers/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Volunteers',
+    'summary': """Volunteer app to manage volunteer and the shop""",
+    'description': """
+        Volunteer Module to manage:
+        - Organize Volunteers
+        - Shop:
+            -Selling local products
+    """,
+    'author': 'EduardoCedillo(eacm)',
+    'category': 'Volunteer',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        
+        
+    ],
+    'demo': [
+        
+    ],
+}
